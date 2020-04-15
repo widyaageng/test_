b6909dc3843ece12ea0188482bfc96e6bd107693
widyaageng <ageng.setyatutuko@gmail.com>
2020-04-15 23:49:03 +0800

    test_utils update: create file loader helper

---
+++ test_utils/helper.py
@@ -0,0 +1,34 @@
+import os
+import pickle
+import torch
+
+
+SPECIAL_WORDS = {'PADDING': '<PAD>'}
+
+
+def load_data(path):
+    """
+    Load Dataset from File
+    """
+    input_file = os.path.join(path)
+    with open(input_file, "r") as f:
+        data = f.read()
+
+    return data
+
+
+def load_preprocess():
+    """
+    Load the Preprocessed Training data and return them in batches of <batch_size> or less
+    """
+    return pickle.load(open('preprocess.p', mode='rb'))
+
+
+def save_model(filename, decoder):
+    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
+    torch.save(decoder, save_filename)
+
+
+def load_model(filename):
+    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
+    return torch.load(save_filename)
